3b76f3a0bf8cf89c17cf7cd879117454c261d9ff
Edgar Ramirez <edgar.ram.rez@outlook.com>
2025-04-18 15:50:54 -0500

    Add React as static template and its Routes/Views/Component for login, logout, register

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 ## server setup
 
 1. Go to server folder
+
 ```bash
 DO IT!
 ```
+
 1. Setup virtual environment
 
 ```bash
@@ -16,12 +18,24 @@ python -m pip install -U -r requirements.txt
 ```
 
 1. Perform migrations
+
 ```bash
 python manage.py makemigrations
 python manage.py migrate
 ```
 
-1. Start local dev server:
+2. Start local dev server:
+
 ```bash
 python manage.py runserver
 ```
+
+3. Create superuser (first time)
+
+```bash
+python manage.py createsuperuser
+```
+
+# Development (frontend)
+
+1. Make changes in `server/frontend` and run `npm run build` because it's configured as TEMPLATES
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,19 +1,17 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import logout
+from django.contrib import messages
+from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -39,27 +37,52 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+def registration(request):
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
+    user_exists = False
+    logger.debug("{} Calling /registration with".format(username))
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+    try:
+        User.objects.get(username=username)
+        user_exists = True
+    except:
+        logger.debug("{} is new user".format(username))
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    logger.debug("{} Calling /registration with user_exists={}".format(username, user_exists))
+    if not user_exists:
+        user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name, email=email)
+        login(request, user)
+        data = {"userName": username, "status": "Authenticated"}
+        return JsonResponse(data)
+    else:
+        return JsonResponse({"userName": username, "status": "Already exists"})
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+    # # Update the `get_dealerships` view to render the index page with
+    # a list of dealerships
+    # def get_dealerships(request):
+    # ...
+
+    # Create a `get_dealer_reviews` view to render the reviews of a dealer
+    # def get_dealer_reviews(request,dealer_id):
+    # ...
+
+    # Create a `get_dealer_details` view to render the dealer details
+    # def get_dealer_details(request, dealer_id):
+    # ...
+
+    # Create a `add_review` view to submit a review
+    # def add_review(request):
+    # ...
