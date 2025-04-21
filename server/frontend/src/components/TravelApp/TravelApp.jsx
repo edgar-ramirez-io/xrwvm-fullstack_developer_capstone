ab8525d12b8af0d096b513c8aef5dcba65f2d8cb
Edgar Ramirez <edgar.ram.rez@outlook.com>
2025-04-21 12:42:38 -0500

    Add a second frontend just for fun

---
+++ server/frontend/src/components/TravelApp/TravelApp.jsx
@@ -0,0 +1,99 @@
+import { sortPlacesByDistance } from "./loc.js";
+import { AVAILABLE_PLACES } from "./data.js";
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+import "./travel.css";
+import logoImg from "./assets/logo.png";
+
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) => {
+  const place = AVAILABLE_PLACES.find((place) => place.id === id) || [];
+  return place;
+});
+
+function TravelApp() {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  useEffect(() => {
+    setTimeout(
+      // FIXME: navigator.geolocation.getCurrentPosition doesn't work in Chrome
+      () => {
+        const sortedPlaces = sortPlacesByDistance(
+          AVAILABLE_PLACES,
+          41.881832, // position.coords.latitude,
+          -87.623177 // position.coords.longitude
+        );
+        setAvailablePlaces(sortedPlaces);
+        console.log({ sortedPlaces }, "useEffect - getCurrentPosition");
+      },
+      500
+    );
+  }, []);
+
+  console.log({ availablePlaces }, "rendered");
+
+  const onClickHandler = (id) => {
+    setPickedPlaces((oldValue) => {
+      if (oldValue.some((place) => place.id === id)) {
+        return oldValue;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...oldValue];
+    });
+    const selectedPlace = AVAILABLE_PLACES.find((place) => place.id === id);
+
+    const placeId = selectedPlace?.id;
+
+    if (placeId === undefined) {
+      return;
+    }
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(placeId) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([placeId, ...storedIds])
+      );
+    }
+  };
+
+  const deleteClickHandler = (id) => {
+    setPickedPlaces((old) => {
+      return old.filter((place) => place.id !== id);
+    });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    const updatedStoreIds = storedIds.filter((place) => place !== id);
+    localStorage.setItem("selectedPlaces", JSON.stringify(updatedStoreIds));
+  };
+
+  return (
+    <>
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          places={pickedPlaces}
+          fallbackText="Pick places where you'd like to go!"
+          onSelectPlace={deleteClickHandler}
+        />
+        <Places
+          title="Available Places"
+          places={availablePlaces}
+          fallbackText="Fetching nearby places..."
+          onSelectPlace={onClickHandler}
+        />
+      </main>
+    </>
+  );
+}
+
+export default TravelApp;
